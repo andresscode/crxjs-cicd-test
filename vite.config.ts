17c9b062f8e064cbf52f99f130e646b14f5c79f3
Andres <21141606+andresscode@users.noreply.github.com>
2023-10-25 23:39:35 -0500

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config.ts'
 
+// feat
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })]
